df62fd12f5dd08da6d38e47eefd27acdb7c563e3
Kirill Kochnev <kirillkochnev@Kirills-Mac-mini.local>
2021-10-22 15:42:08 +0100

    Updates

---
+++ precache-manifest.e3130cf6da3984950cf8cc9c3b7ecc4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3bcdc47d47d42fb94f9b3ea1c8ea37",
+    "url": "/crypto_mintable_token_sale/index.html"
+  },
+  {
+    "revision": "dc5d7afb093cf3ad88fb",
+    "url": "/crypto_mintable_token_sale/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "105f65a5c16b38ef778e",
+    "url": "/crypto_mintable_token_sale/static/js/2.6e222179.chunk.js"
+  },
+  {
+    "revision": "dc5d7afb093cf3ad88fb",
+    "url": "/crypto_mintable_token_sale/static/js/main.d914e2c2.chunk.js"
+  },
+  {
+    "revision": "551f632892c90a5bacd6",
+    "url": "/crypto_mintable_token_sale/static/js/runtime-main.2936e478.js"
+  }
+]);
